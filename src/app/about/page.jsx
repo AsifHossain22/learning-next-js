8dc65b0cab79edaa66322d1173a69bce153155c9
AsifHossain22 <only1asif4you22@gmail.com>
2025-12-23 11:57:50 +0400

    Learned about Linking Navigating

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function AboutPage() {
+  const router = useRouter();
+  // const isLoggedIn = true;
+  const isLoggedIn = false;
+  const handleNavigation = () => {
+    if (isLoggedIn) {
+      router.push("/about/education");
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
     <div>
-      <h1 className="text-5xl">About Page</h1>
+      <h1 className="text-5xl text-center">About Page</h1>
+
+      <p>
+        <Link href="/about/education" className="text-blue-400">
+          Click here
+        </Link>
+        to check out my education details
+      </p>
+
+      <button
+        type="button"
+        onClick={handleNavigation}
+        className="hover:cursor-pointer"
+      >
+        Go to my education
+      </button>
     </div>
   );
 }
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
   return (
     <nav className="flex justify-center">
       <ul className="flex justify-between w-1/2">
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Contact</li>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="/services">
+          <li>Services</li>
+        </Link>
+        <Link href="/contact">
+          <li>Contact</li>
+        </Link>
       </ul>
     </nav>
   );
